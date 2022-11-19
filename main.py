8bdd925d57f1c1a2bdde8f810d90f9502b069745
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-11-19 10:53:52 +0200

    bitoc

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ print(f"Datatype - {type(response.text)}")
 
 response = requests.post('https://httpbin.org/post', data='Test', headers = {'h1': 'TITLE'})
 print(response.text)
+#КУРС БІТКА
+res_parse_list = []
+response = requests.get('https://coinmarketcap.com/')
+response_text = response.text
+response_parse = response_text.split("<span>" )
+for pars_elem_1 in response_parse:
+    if pars_elem_1.startswith('$'):
+        for pars_elem_2 in pars_elem_1.split("</span>" ):
+           if pars_elem_2.startswith('$') and pars_elem_2[1].isdigit():
+               res_parse_list.append(pars_elem_2)
+bitcoin = res_parse_list[0]
+print(bitcoin)
+
+
 
 
 
